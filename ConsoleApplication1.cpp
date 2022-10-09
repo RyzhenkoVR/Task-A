37654788d40276a05747cb3dc1a0bdaf49f3400d
RyzhenkoVR <115408602+RyzhenkoVR@users.noreply.github.com>
2022-10-09 16:37:39 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+﻿// Ryzhenko V.R.
+// task 1
+// 09.10.2022
+#include <iostream>
+using namespace std;
+int main()
+{
+	int A, B, C;
+	cin >> A;
+	C = A % 10;
+	C *= 10;
+	B = A / 10;
+	cout << C <<"+" << B << "=" << C+B;
+}
